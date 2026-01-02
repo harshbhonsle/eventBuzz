3ec9324c46b7232a274d3b13fa5b2bf6aa4d8472
Harsh <bhonsleharsh16@gmail.com>
2026-01-02 22:28:42 +0530

    Add cookie to logout , login and register

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -14,6 +14,20 @@ export const register = asyncHandler(async(req:Request, res:Response)=>{
     const hashedPassword = await bcrypt.hash(password,10);
     const user = new User({email, password:hashedPassword,role});
     await user.save();
+    // token 
+    const token = jwt.sign(
+        {id:user._id, role:user.role},
+        process.env.JWT_SECRET as string,
+        {expiresIn: "1h"}
+    )
+
+    // Local dev: no HTTPS, so secure=false
+    res.cookie("authToken", token,{
+        httpOnly:true,
+        secure:false, //false for local dev , true for prod,
+        sameSite: "lax",
+        maxAge: 60 * 60 * 1000,
+    })
     
     res.status(201).json({message:"User registered successfully"});
 })
@@ -39,5 +53,20 @@ export const login = asyncHandler(async(req:Request, res:Response)=>{
         process.env.JWT_SECRET as string, 
         { expiresIn: "1h" }
     )
-    res.json({ token });
+    res.cookie("authToken",token,{
+        httpOnly:true,
+        secure:false, // false for local dev and true for prod
+        sameSite:"lax",
+        maxAge: 60 * 60 * 1000,
+    })
+    res.json({ message: "Login successful" });
+})
+
+export const logout = asyncHandler(async(req:Request, res:Response)=>{
+    res.clearCookie("authToken",{
+        httpOnly:true,
+        secure:false,
+        sameSite:"lax", // for local false, 
+    })
+    res.json({ message: "Logged out successfully" });
 })
